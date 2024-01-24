9b89d4691411dad72d1ee86c231142ad527f1547
pouyasadri <pouyapaghandeh78@gmail.com>
2024-01-24 20:42:24 +0100

    Initial Commit

---
+++ ShirnganAPp/ShirnganAPpApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShirnganAPpApp.swift
+//  ShirnganAPp
+//
+//  Created by Pouya Sadri on 24/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShirnganAPpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
